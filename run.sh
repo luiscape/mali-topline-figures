0d11e8bc857ca32af02f34b9f2c9076029402b37
Luis Capelo <luiscape@gmail.com>
2015-04-15 13:35:29 -0400

    adding stag scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-FILE_PATH="code/create_datastore_stag.py"
-TEMP_PATH="data/temp.csv"
+# File paths.
+FILE_PATH="tool/code/create_datastore.py"
+STAG_PATH="tool/code/create_datastore_stag.py"
+TEMP_PATH="tool/data/temp.csv"
+
 # Activating the DataStore
 source venv/bin/activate
 python $FILE_PATH $HDX_KEY $TEMP_PATH
+python $STAG_PATH $HDX_KEY $TEMP_PATH
