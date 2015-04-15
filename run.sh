a96e26823a1c9339f64690a655d4f726e1969884
Luis Capelo <luiscape@gmail.com>
2015-04-15 13:32:47 -0400

    adding stating scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FILE_PATH="code/create_datastore_stag.py"
+TEMP_PATH="data/temp.csv"
 # Activating the DataStore
 source venv/bin/activate
-python tool/code/create_datastore.py API_KEY tool/data/temp.csv
+python $FILE_PATH $HDX_KEY $TEMP_PATH
